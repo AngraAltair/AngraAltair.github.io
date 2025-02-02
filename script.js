f9853cf9169f198e7845f814654deb3f8f4a32ec
SHERWIN JOHN V. TARQUINI <149936737+AngraAltair@users.noreply.github.com>
2025-02-02 17:53:34 +0800

    sdfeef

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateGreeting() {
+    const now = new Date();
+    const hours = now.getHours();
+    let greeting;
+
+    if (hours < 12) {
+        greeting = "Good morning!";
+    } else if (hours < 18) {
+        greeting = "Good afternoon!";
+    } else {
+        greeting = "Good evening!";
+    }
+
+    document.getElementById("greeting").textContent = greeting;
+}
+
+window.onload = updateGreeting;
